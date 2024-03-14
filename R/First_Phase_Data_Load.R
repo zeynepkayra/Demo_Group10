28c81e8f014e1118eb96a5aa37f087f342d4257a
Zeynep Kayra Ozcakar <kayra@Zeyneps-MacBook-Air-4.local>
2024-03-14 14:43:32 +0000

    Data for Second Phase Data Load is created

--- R/First_Phase_Data_Load.R
+++ R/First_Phase_Data_Load.R
@@ -372,12 +372,10 @@ dbWriteTable(my_db, "joint_order", joint_order, overwrite= TRUE)
 
 # Calculation of Total Price of Orders after Discounts are applied
 create_price_view <- '
-
 CREATE VIEW IF NOT EXISTS final_price_of_order AS select order_detail_id, subtotal, discount, subtotal*(1-discount*0.01) as final_price from 
 (select A.order_detail_id, count(A.product_id) as no_of_products, sum(A.total_price_for_each_product) as subtotal, O.discount from 
 (select J.order_detail_id, J.product_id, P.price, J.quantity, (P.price* J.quantity) as total_price_for_each_product from joint_order J join product p where J.product_id = P.product_id) A 
-join order_detail O on A.order_detail_id = O.order_detail_id group by A.order_detail_id); 
-
+join order_detail O on A.order_detail_id = O.order_detail_id group by A.order_detail_id);
 '
 
 dbExecute(schema_db, create_price_view)
