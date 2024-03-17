67c31ab11aef0b883c1b62c09705e30ff8f5e65b
zeynepkayra <67283468+zeynepkayra@users.noreply.github.com>
2024-03-17 15:19:39 +0000

    Update DataAnalysis.R

--- Data_Analysis/DataAnalysis.R
+++ Data_Analysis/DataAnalysis.R
@@ -1,7 +1,3 @@
-## ----setup, include=FALSE-----------------------------------------------
-knitr::opts_chunk$set(echo = TRUE)
-
-
 ## -----------------------------------------------------------------------
 library(RSQLite)
 library(dplyr)
